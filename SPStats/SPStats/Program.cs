37ffc82b7d051cc7310a4e6995d5aeb8c760f993
Vardhaman Deshpande <vardhaman3230@gmail.com>
2012-02-26 16:24:52 +0530

    second commit

---
+++ SPStats/SPStats/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint.Administration;
+using Microsoft.SharePoint;
+
+namespace SPStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Microsoft.SharePoint.Administration.SPAdministrationWebApplication centralWeb = SPAdministrationWebApplication.Local;
+            //Console.WriteLine(centralWeb.DisplayName);
+
+            foreach (SPService curService in SPFarm.Local.Services)
+            {
+                if (curService is SPWebService)
+                {
+                    SPWebService webService = (SPWebService)curService;
+
+                    foreach (SPWebApplication webApp in webService.WebApplications)
+                    {
+                        Console.WriteLine(webApp.DisplayName);
+
+                        foreach (SPSite site in webApp.Sites)
+                        {
+                            foreach(SPWeb web in site.AllWebs)
+                            {
+                                Console.WriteLine(web.Title);
+
+                                foreach (SPWeb subWeb in web.Webs)
+                                {
+                                    Console.WriteLine(subWeb.Title);
+                                }
+                            }
+                            
+                        }
+                    }
+                }
+            }
+
+            
+            Console.ReadKey();
+        }
+    }
+}
