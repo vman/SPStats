67cba3ec46ad577ed1501b6cb85b7e26cf71c8e7
Vardhaman Deshpande <vardhaman3230@gmail.com>
2012-03-11 14:51:08 +0530

    started on the MVVM patter. Added the ViewModel and the Model

---
+++ SPStats/SPStats.WPF/FarmViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SPStats.WPF.Models;
+
+namespace SPStats.WPF
+{
+    public class FarmViewModel
+    {
+        Solution solution;
+
+        public string SolutionName
+        {
+            get { return solution.SolutionName;}
+            set { solution.SolutionName = value; }
+        }
+    }
+}
---
+++ SPStats/SPStats.WPF/Models/Solution.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SPStats.WPF.Models
+{
+    public class Solution
+    {
+        public string SolutionName { get; set; }
+    }
+}
